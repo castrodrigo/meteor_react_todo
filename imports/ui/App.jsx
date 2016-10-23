e9ed5bbdb3966b177964c20994298948b0f1eea7
Rodrigo de Castro <castrodrigo@yahoo.com.br>
2016-10-23 12:26:08 -0200

    adding PropTypes call

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -10,6 +10,19 @@ class App extends Component {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
   }
 }
 
