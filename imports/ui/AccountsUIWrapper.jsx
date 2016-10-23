694f72d3e42df2a666393bb1b0b8f73030a58da0
Rodrigo de Castro <castrodrigo@yahoo.com.br>
2016-10-23 20:43:02 -0200

    Adding patches, native bcrypt, user control, blaze templating to meteor auth

---
+++ imports/ui/AccountsUIWrapper.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
+export default class AccountsUIWrapper extends Component {
+  componentDidMount() {
+    // Meteor Blaze to render login buttons
+    this.view = Blaze.render(Template.loginButtons,
+      ReactDOM.findDOMNode(this.refs.container));
+  }
+
+  componentWillUnmount() {
+    // Clean up Blaze view
+    Blaze.remove(this.view);
+  }
+
+  render() {
+    // Render a placeholder container
+    return <span ref="container" />;
+  }
+
+}
