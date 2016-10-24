08b577e7a360512e03a9c4f2e2cee4e7bcb9643c
Rodrigo de Castro <castrodrigo@yahoo.com.br>
2016-10-23 22:34:02 -0200

    adding some tests

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,49 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+import { Tasks } from './tasks.js';
+
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    describe('methods', () => {
+      const userId = Random.id();
+      let taskId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+        taskId = Tasks.insert({
+          text: 'test task',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'tmeasday',
+        });
+      });
+
+      it('can delete owned task', () => {
+        // Finding the internal implementation of the task method to test it in isolation
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+        // Fake method invocation that looks like what the method expects
+        const invocation = { userId };
+        // Running the method with `this` set to the fake invocation
+        deleteTask.apply(invocation, [taskId]);
+        // Verifying that the method does what is expected
+        assert.equal(Tasks.find().count(), 0);
+      });
+
+      it('can make owned task private', () => {
+        // Finding the internal implementation of the task method to test it in isolation
+        const privateTask = Meteor.server.method_handlers['tasks.setPrivate'];
+        const setToPrivate = true;
+        // Fake method invocation that looks like what the method expects
+        const invocation = { taskId, setToPrivate };
+        // Running the method with `this` set to the fake invocation
+        privateTask.apply(invocation, [taskId]);
+        // Verifying that the method does what is expected
+        assert.equal(Tasks.findOne(taskId).private, true);
+      });
+    });
+  });
+}
